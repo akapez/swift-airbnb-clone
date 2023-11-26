7509814428eb7b788a75ef23984e25992b19a5e5
Avishka <avishkakapuruge@gmail.com>
2023-11-26 18:56:41 +0530

    listing update

---
+++ Airbnb/Core/Listings/ListingItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingItemView.swift
+//  Airbnb
+//
+//  Created by Avishka Kapuruge on 2023-11-26.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
