756522b497c157fe59433596c5ca974ed0608c30
Avishka <avishkakapuruge@gmail.com>
2023-11-26 19:42:45 +0530

    update search bar

---
+++ Airbnb/Core/Explore/SearchAndFilterbar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchAndFilterbar.swift
+//  Airbnb
+//
+//  Created by Avishka Kapuruge on 2023-11-26.
+//
+
+import SwiftUI
+
+struct SearchAndFilterbar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchAndFilterbar()
+}
