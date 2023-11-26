76992f06c9e64b67e8cfaebe5212003fa344be71
Avishka <avishkakapuruge@gmail.com>
2023-11-26 18:19:57 +0530

    Initial Commit

---
+++ Airbnb/App/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Avishka Kapuruge on 2023-11-26.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Airbnb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Airbnb
+//
+//  Created by Avishka Kapuruge on 2023-11-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
